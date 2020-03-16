dbc2b88331f50aca1af3f716d25df850e967dea7
dangxing <dangxing@163.com>
2020-03-16 10:56:38 +0800

    '云音乐开发'

---
+++ miniprogram/components/paylist/paylist.js
@@ -0,0 +1,31 @@
+// components/paylist/paylist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playlist:{
+      type:Object
+    }
+  },
+  observers:{
+    ['playlist.playCount'](val){
+      console.log(val)
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
